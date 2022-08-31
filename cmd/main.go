b4b178beabf076c0584350386c7093db7e278e37
Tim Richardson <trichardsonjr@gmail.com>
2022-08-30 22:45:04 -0700

    wip

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/spf13/viper"
 	"github.com/trichardsonjr78/go-gin-api-medium/pkg/books"
 	"github.com/trichardsonjr78/go-gin-api-medium/pkg/common/db"
+	"github.com/trichardsonjr78/go-gin-api-medium/pkg/terraform_api"
 )
 
 func main() {
@@ -18,6 +19,7 @@ func main() {
 	h := db.Init(dbUrl)
 
 	books.RegisterRoutes(r, h)
+	terraform_api.RegisterRoutes(r, h)
 	//db.Init(dbUrl)
 
 	// r.GET("/", func(c *gin.Context) {
